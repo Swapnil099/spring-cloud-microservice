4cf38f8dff9ac591085e4ef6ee553c21d2718263
swapnil gawande <gswapnil074@gmail.com>
2022-10-05 19:45:19 +0530

    added rating data api and made API calls between microservices without using service discovery server

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,16 +26,22 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        System.out.println("its here");
+        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/"+userId, UserRating.class);
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("123",4),
-                new Rating("124",5)
-        );
-
-        return ratings.stream().map(rating -> {
+        return userRating.getRatings().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
             return new CatalogItem(movie.getName(),"Desc",rating.getRating());
         }).collect(Collectors.toList());
 
     }
 }
+
+// Async request calls to APIs using webflux dependecies
+// Movies movie = WebClientBuilder.build()
+//         .get()
+//         .url("http://localhost:8082/movies/"+rating.getMovieId())
+//         .retrieve()
+//         .bodyToMono(Movie.class)
+//         .block() // block is making this call synchronous otherwise it will remain async
+
--- rating-data-service/src/main/java/com/example/ratingdataservice/resources/RatingResource.java
+++ rating-data-service/src/main/java/com/example/ratingdataservice/resources/RatingResource.java
@@ -1,16 +1,32 @@
 package com.example.ratingdataservice.resources;
 
 import com.example.ratingdataservice.models.Rating;
+import com.example.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResource {
 
-    @RequestMapping("/movies/{movieId}")
+    @RequestMapping("/{movieId}")
     public Rating movieRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId,4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("99",4),
+                new Rating("100",5)
+        );
+        UserRating userRating = new UserRating(ratings);
+        return userRating;
     }
 }
