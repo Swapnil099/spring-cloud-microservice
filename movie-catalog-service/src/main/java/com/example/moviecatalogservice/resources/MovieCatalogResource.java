157e6154664feb9594b5cb4485959bb88a8315b8
swapnil gawande <gswapnil074@gmail.com>
2022-10-06 12:24:55 +0530

    converted services to eureka clients

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -27,10 +27,10 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         System.out.println("its here");
-        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/"+userId, UserRating.class);
+        UserRating userRating = restTemplate.getForObject("http://movie-rating-service/ratingsdata/user/"+userId, UserRating.class);
 
         return userRating.getRatings().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(),Movie.class);
             return new CatalogItem(movie.getName(),"Desc",rating.getRating());
         }).collect(Collectors.toList());
 
